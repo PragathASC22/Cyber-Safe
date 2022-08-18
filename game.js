8736d45f13789fd7602c68c904e03bfb35488ed9
PragathASC22 <pragathtsiva@gmail.com>
2022-08-18 13:25:13 -0400

    8-18 Commit

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let score = document.getElementById("score");
 let percent = document.getElementById("percent");
 
 let points = 0;
-let num_email = 0;
+let num_email = 1;
 
 
 let fake_button = document.getElementById("fake_button");
@@ -23,18 +23,17 @@ next_email = function() {
     email_display.innerHTML = emails[randomIdx].content;
     email_title.innerHTML = emails[randomIdx].title;
     email_sender.innerHTML = emails[randomIdx].email;
-    status_display.innerHTML = emails[randomIdx].status_F_R;
+    status_display.innerHTML = "";
 
     random_num = randomIdx;
-};
+}
 
 updatepoints = function() {
     score.innerHTML = points + "/" + num_email;
-    percent.innerHTML = (points/num_email).toFixed(2) + "%";
-};
+    percent.innerHTML = (points*100/num_email).toFixed(0) + "%";
+}
 
 fake_button.onclick = function() {
-    next_email();
     if (email_display.innerHTML == emails[0].content) {
         points += 1;
     }
@@ -42,21 +41,24 @@ fake_button.onclick = function() {
         points += 1;
     }
     updatepoints();
-};
+    next_email();
+}
 
 
 real_button.onclick = function() {
-    next_email();
     if (email_display.innerHTML == emails[0].content) {
         console.log("pass")
     }
     if (emails[random_num].status_F_R == "Real") {
         points += 1;
-    };
+    }
     updatepoints();
-};
+    next_email();
+}
 
 email_display.innerHTML = emails[random_num].content;
 email_title.innerHTML = emails[random_num].title;
 email_sender.innerHTML = emails[random_num].email;
-status_display.innerHTML = emails[random_num].status_F_R;
+// status_display.innerHTML = emails[random_num].status_F_R;
+status_display.innerHTML = "";
+
